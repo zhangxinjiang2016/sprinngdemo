8bb872644bd30b12f040e7ee6b1bb482669c45e2
张新江2016 <582889573@qq.com>
2020-07-21 23:15:28 +0800

    测试内容

---
+++ src/main/java/com/test/sprinngdemo/SprinngdemoApplication.java
@@ -0,0 +1,13 @@
+package com.test.sprinngdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinngdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinngdemoApplication.class, args);
+    }
+
+}
